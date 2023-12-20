aba4dde614a58eb14b6f8b9d47c4c689f4657741
Nguyen Nhat Quang <84659729+nhatquang237@users.noreply.github.com>
2023-12-20 09:44:21 +0700

    [FIX] Bug not get fully database uri - Del the cache of URI in environ dictionary

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
+import os
 from decouple import AutoConfig, Csv
-
 config = AutoConfig()
 
 PORT = int(config("PORT"))
@@ -10,5 +10,9 @@ ALGORITHM = config("ALGORITHM")
 TOKEN_EXPIRE_MINUTES = int(config("TOKEN_EXPIRE_MINUTES"))
 
 # MongoDB URI
+if "MONGODB_URI" in os.environ:
+    # To make sure the value of uri always up to date with .env file
+    del os.environ["MONGODB_URI"]
 MONGODB_URI = config("MONGODB_URI")
+
 ORIGINS = config("ORIGINS", cast=Csv())
